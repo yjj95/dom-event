043bb5c12809577d52094c0cd84a486881d39e3e
jia <15901226963@163.com>
2018-11-23 22:14:21 +0800

    节省内存

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-/*$('#button').on("click",function(e){
-   $(".popover").show()
-   e.stopPropagation()
-})
-//解决点击浮层，浮层消失,点击浮层不触发函数
-$(".popover").on("click",function(e){
-   $(".popover").show()
-   e.stopPropagation()
-})*/
+//$(".wrapper").on('click',false)//阻止传播和默认事件
+//如果有很多个popover都需要监听，是很耗费内存的
+//节省内存
 $(".wrapper").on("click",function(e){
    $(".popover").show()
+   $(document).one("click",function(){
+      $(".popover").hide()
+   })
    //阻止事件冒泡
-   e.stopPropagation()
-})
-$(document).on("click",function(){
-   $(".popover").hide()
+   e.stopPropagation()//IE8会阻止checkbox的默认事件
 })
